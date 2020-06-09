2759968accfa7282f0d7eb6d9621d2928da3fa1b
Taro YABUKI <taroyabuki@users.noreply.github.com>
2020-06-09 11:18:47 +0900

    PHP

---
+++ hello2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel='stylesheet' href='style.css' />
+    <title>タイトル</title>
+  </head>
+  <body>
+    <p>計算結果は<?php echo(1 + 1); ?>です．</p>
+  </body>
+</html>
