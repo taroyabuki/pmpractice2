562f317467e829bab2cc1a8242c0d6c2a360ae96
Taro Yabuki <taroyabuki@users.noreply.github.com>
2022-04-27 00:25:17 +0900

    Update db.php

--- db.php
+++ db.php
@@ -9,7 +9,7 @@ function h($var) {
   }
 }
 
-$dbServer = '127.0.0.1';
+$dbServer = isset($_ENV['MYSQL_SERVER'])    ? $_ENV['MYSQL_SERVER']      : '127.0.0.1';
 $dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'testuser';
 $dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'pass';
 $dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
